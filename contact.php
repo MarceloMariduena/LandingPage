38fe9bf5eb6d43fb43ecff26d854a1edce91e817
root <root@localhost.localdomain>
2019-05-13 13:54:09 -0400

    Updates

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['e-mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = 'marcelomariduena@protonmail.com';
+    
+
+}
+
+
+
+?>
